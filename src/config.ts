fbd5233bb0cf70d397a09d17c902fa5f8668e49c
Adam Leis <aleis@iti4dmv.com>
2024-05-21 22:03:56 -0400

    update site config & global styles

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Adam Leis";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Welcome to my blog! I write about my polymath adventures, which includes web development, philosophy, theology, apologetics, science, and more.";
+export const TWITTER_HANDLE = "@theNthAdam";
+export const MY_NAME = "Adam";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
